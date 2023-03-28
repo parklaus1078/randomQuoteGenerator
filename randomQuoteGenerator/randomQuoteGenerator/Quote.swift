bb679288a25f484b9e7a97a1cdba1d1cd54b4c7d
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2023-03-28 22:56:52 +0900

    [ADD]: added functionality of generating new quote, when the button is clicked

---
+++ randomQuoteGenerator/randomQuoteGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  randomQuoteGenerator
+//
+//  Created by Geun Woo Park on 2023/03/28.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
--- randomQuoteGenerator/randomQuoteGenerator/ViewController.swift
+++ randomQuoteGenerator/randomQuoteGenerator/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let quotes = [
+        Quote(contents: "남에겐 관대하게, 나에겐 야박하게", name: "박근우"),
+        Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌"),
+        Quote(contents: "분노는 바보들의 가슴속에서만 살아간다.", name: "아인슈타인"),
+        Quote(contents: "몇 번이라도 좋다! 이 끔찍한 생이여...다시!", name: "니체"),
+        Quote(contents: "인생은 멀리서 보면 희극이고 가까이서 보면 비극이다.", name: "찰리 채플린")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapQuoteGenerator(_ sender: Any) {
+        let random = Int(arc4random_uniform(5)) // creates a random real number between 0 and 4
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.contents
+        self.nameLabel.text = quote.name
+    }
 }
 
